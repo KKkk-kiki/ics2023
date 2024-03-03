6437d75783345177f3a6a0c3dbf400c59866636b
tracer-ics2023 <tracer@njuics.org>
2024-03-03 19:25:01 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:25:01 up 1 day,  2:47,  1 user,  load average: 0.57, 0.67, 0.66

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,7 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for(int i = 0; i < 32 ; i++){
-    printf("reg %s : %u\n",regs[i],cpu.gpr[i]);
+    printf("reg %-3s : %u\n",regs[i],cpu.gpr[i]);
   }
   
 }
