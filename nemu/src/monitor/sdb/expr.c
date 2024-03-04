deb686a7b1c49d5a5ee6edc5d35f5bac381e4ec4
tracer-ics2023 <tracer@njuics.org>
2024-03-04 17:26:53 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:26:53 up  7:08,  1 user,  load average: 0.66, 0.56, 0.55

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -22,8 +22,11 @@
 #include <string.h>
 #include <stdbool.h>
 enum {
-  TK_NOTYPE = 256, TK_EQ, TK_INT
-
+  TK_NOTYPE = 256, 
+  REG = 1, 
+  HEX = 2, 
+  NUM = 3,
+  TK_EQ, 
   /* TODO: Add more token types */
 
 };
@@ -38,16 +41,22 @@ static struct rule {
    */
 
   {" +", TK_NOTYPE},    // spaces
+
   {"\\+", '+'},         // plus    
-  {"-", '-'}, 
+  {"-", '-'},           //sub
+  {"\\*", '*'},         //mul
+  {"/", '/'},           //div
+
+  {"\\(", '('},             // LEFT
+  {"\\)", ')'},             //RIGHT
+
+  {"\\$[a-zA-Z0-9]+",REG},      //Reg
+  {"0[xX][0-9a-fA-F]+",HEX},    //HEX
+  {"[0-9]*",NUM},               //Num
+
   {"==", TK_EQ},        // equal
 
-//order?
-  {"\\(", '('},        // 
-  {"\\)", ')'}, 
-  {"\\*", '*'}, 
-  {"/", '/'}, 
-  {"\\d+", TK_INT}, 
+
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -102,11 +111,39 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-        tokens->type = rules[i].token_type;
-        strncpy(tokens->str, e + position, substr_len);
-        
-
         switch (rules[i].token_type) {
+          case TK_NOTYPE:
+            break;
+          case '+':
+            tokens[nr_token++].type = rules[i].token_type;
+            break;
+          case '-':
+            tokens[nr_token++].type = rules[i].token_type;
+            break;
+          case '*':
+            tokens[nr_token++].type = rules[i].token_type;
+            break;
+          case '/':
+            tokens[nr_token++].type = rules[i].token_type;
+            break;
+          case '(':
+            tokens[nr_token++].type = rules[i].token_type;
+            break;
+          case ')':
+            tokens[nr_token++].type = rules[i].token_type;
+            break;
+          case REG:
+            strncpy(tokens[nr_token].str, e + position - substr_len, substr_len);
+            tokens[nr_token++].type = rules[i].token_type;
+            break; 
+          case HEX:
+            strncpy(tokens[nr_token].str, e + position - substr_len, substr_len);
+            tokens[nr_token++].type = rules[i].token_type;
+            break;
+          case NUM:
+            strncpy(tokens[nr_token].str, e + position - substr_len, substr_len);
+            tokens[nr_token++].type = rules[i].token_type;
+            break;         
           default: TODO();
         }
 
@@ -176,17 +213,56 @@ uint32_t eval(int p, int q){
   }
   else {
     //op = the position of 主运算符 in the token expression;
-    int op;
+    int op = 0;
+    int op_type = 0;
+    int prior ;
+    int mini_prior = 3;
+    int in_parentheses = 0;
+    for(int i = p; i <= q; i++){
+      switch (tokens[i].type)
+      {
+      case '+':
+        prior = 1;
+        break;
+      case '-':
+        prior = 1;
+        break;
+      case '*':
+        prior = 2;
+        break;
+      case '/':
+        prior = 2;
+        break;
+      case '(':
+        in_parentheses = 1;
+        prior = 3;
+        break;
+      case ')':
+        in_parentheses = 0;
+        prior = 3;
+        break;
+      default:
+        prior = 3;
+        break;
+      }
+      if(in_parentheses){
+        prior = 3;
+      }
+      if(prior <= mini_prior){
+        op = i;
+        op_type = tokens[i].type;
+      }
+    }
     int val1 = eval(p, op - 1);
     int val2 = eval(op + 1, q);
 
-    // switch (op_type) {
-    //   case '+': return val1 + val2;
-    //   case '-': return val1 - val2;
-    //   case '*': return val1 * val2;
-    //   case '/': return val1 / val2;
-    //   default: assert(0);
-    // }
+    switch (op_type) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': return val1 / val2;
+      default: assert(0);
+    }
   }
 
   return 0;
@@ -199,7 +275,8 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-
+  uint32_t result = eval(0,nr_token-1);
+  printf("%u\n",result);
   /* TODO: Insert codes to evaluate the expression. */
   TODO();
 
