a69eed3fce391c3992f3dd86cad7e06250d0d4dd
tracer-ics2023 <tracer@njuics.org>
2024-03-05 19:59:47 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:59:47 up 55 min,  1 user,  load average: 0.62, 0.35, 0.30

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -262,7 +262,7 @@ uint32_t eval(int p, int q){
       case '-': return val1 - val2;
       case '*': return val1 * val2;
       case '/': return val1 / val2;
-      default: assert(0);
+      //default: //assert(0);
     }
   }
 
