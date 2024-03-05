6a1a3906200fdba487e0f6bd7b2ca50611c41988
tracer-ics2023 <tracer@njuics.org>
2024-03-05 19:59:20 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:59:20 up 55 min,  1 user,  load average: 0.66, 0.33, 0.30

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -256,7 +256,7 @@ uint32_t eval(int p, int q){
     }
     int val1 = eval(p, op - 1);
     int val2 = eval(op + 1, q);
-
+    printf("%d",op);
     switch (op_type) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
