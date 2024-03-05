d2c345255f261808418c226d46115eab8144ab21
tracer-ics2023 <tracer@njuics.org>
2024-03-05 19:25:39 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:25:39 up 21 min,  1 user,  load average: 0.43, 0.50, 0.45

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -99,6 +99,7 @@ static bool make_token(char *e) {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+        printf("%d\n",rules[i].token_type);
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
