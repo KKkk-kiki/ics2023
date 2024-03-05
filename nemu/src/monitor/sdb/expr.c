f40220527927c8cd2374be7a958c7cd38d71a8cf
tracer-ics2023 <tracer@njuics.org>
2024-03-05 20:38:16 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:38:16 up  1:34,  1 user,  load average: 0.41, 0.47, 0.39

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -207,7 +207,7 @@ uint32_t eval(int p, int q){
      */
     uint32_t num = 0;
     sscanf(tokens[p].str,"%u",&num);
-    printf("%u",num);
+    printf("%u\n",num);
     return num;
   }
   else if (check_parentheses(p, q) == true) {
