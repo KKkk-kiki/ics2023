b1a25d9af3c9a460fc33d89f789e694879363cd3
tracer-ics2023 <tracer@njuics.org>
2024-03-07 14:55:06 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:55:06 up 1 day,  4:34,  1 user,  load average: 0.44, 0.91, 0.92

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -40,7 +40,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
     if(strcmp(s,regs[i]) == 0)
     *success = true;
     reg_val = cpu.gpr[i];
-    printf("%s\n",regs[i]);
+    printf("%d\n",strcmp(s,regs[i]));
   }
   if(i == regs_len){
     *success = false;
