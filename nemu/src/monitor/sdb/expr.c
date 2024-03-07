efa026e458db47224ba0719a22b9be89bffba407
tracer-ics2023 <tracer@njuics.org>
2024-03-07 14:51:07 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:51:07 up 1 day,  4:30,  1 user,  load average: 1.17, 1.28, 0.99

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -142,6 +142,7 @@ static bool make_token(char *e) {
           case REG:
           //REG应该转后再传值
             strncpy(reg_temp, e + position - substr_len + 1, substr_len - 1);//滤掉$
+            printf("%s\n",reg_temp);
             sprintf(tokens[nr_token].str, "%u", isa_reg_str2val(reg_temp,&reg_flag));
             tokens[nr_token++].type = rules[i].token_type;
             break; 
