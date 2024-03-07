dfa1fa2d170b281e8a7cc368302a7570ff22cac9
tracer-ics2023 <tracer@njuics.org>
2024-03-07 15:07:26 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:07:26 up 1 day,  4:46,  1 user,  load average: 0.87, 0.92, 0.94

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -316,6 +316,7 @@ word_t expr(char *e, bool *success) {
   for (i = 0; i < nr_token; i ++) {
   if (tokens[i].type == '*' && (i == 0 || tokens[i - 1].type == '+') ) {
     tokens[i].type = DEREF;
+    printf("deref");
   }//情况待拓展
 }
   uint32_t result = eval(0,nr_token-1);
