2ebccd9d4579a743156a528b416d3867130cd0b6
tracer-ics2023 <tracer@njuics.org>
2024-03-08 17:03:00 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:03:00 up  6:53,  1 user,  load average: 0.68, 0.57, 0.50

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -18,8 +18,9 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
-//#include "monitor/sdb/sdb.h"
-//char test[1000] = "(0XA + 3)==10";bool success;   //仅测试用，已优化寄存器和十六进制传值，待测试（指针解引用待测试）,暂时跳过测试，进入PA2
+
+#include "monitor/sdb/sdb.h"
+char test[1000] = {""};bool success;   //仅测试用，已优化寄存器和十六进制传值，待测试（指针解引用待测试）,暂时跳过测试，进入PA2
 //#include <isa.h>
 //cpu.gpr[0]=1;
 int main(int argc, char *argv[]) {
@@ -32,7 +33,10 @@ int main(int argc, char *argv[]) {
 
   /* Start engine. */
   engine_start();
-  
+  int i;
+  i = 11;
+  printf("%p\n",&i);
+  //expr("1+&i", &success);
   //expr(test, &success);
 
   return is_exit_status_bad();
