bb3ef67d7eaabecebd3feb7a479b32af4267644b
tracer-ics2023 <tracer@njuics.org>
2024-03-14 15:14:42 +0800

    >  compile NEMU 2579373189 kk Linux KK 5.15.0-100-generic #110~20.04.1-Ubuntu SMP Tue Feb 13 14:25:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:14:42 up 1 min,  1 user,  load average: 1.02, 0.39, 0.14

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,31 +5,101 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  const char *str = s;
+  while(*str != '\0'){
+    str++;
+  }
+  return str - s;
+  //panic("Not implemented");
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *ret = dst;
+  if ((dst == NULL) || (src == NULL)){
+    return NULL;
+  }
+  while(*src != '\0'){
+    *dst = *src;
+    dst++;
+    src++;
+  }
+  *dst = '\0';
+  return ret;
+  //panic("Not implemented");
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *ret = dst;
+  if ((dst == NULL) || (src == NULL)){
+    return NULL;
+  }
+  while(n > 0){
+    *dst = *src;
+    dst++;
+    src++;
+    n--;
+  }
+  *dst = '\0';
+  return ret;
+  //panic("Not implemented");
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+    char *ptr = dst;
+    if ((dst == NULL) || (src == NULL)){
+      return NULL;
+    }
+    // 找到 dest 字符串的末尾
+    while (*ptr != '\0') {
+        ptr++;
+    }
+    // 将 src 字符串的内容追加到 dest 的末尾
+    while (*src != '\0') {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
+    // 确保 dest 字符串以空字符结尾
+    *ptr = '\0';
+    return dst;
+  //panic("Not implemented");
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+
+  unsigned char c1, c2;
+  do{
+      c1 = (unsigned char) *s1++;
+      c2 = (unsigned char) *s2++;
+      if (c1 == '\0')
+	return c1 - c2;
+  }while (c1 == c2);
+  
+  return c1 - c2;
+  //panic("Not implemented");
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  if (s1 == NULL || s2 == NULL) {
+    return 0;
+  }
+  while (n != 0 && *s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
+    --n;
+    ++s1;
+    ++s2;
+  }
+  // 当比较了n次后，即新n变为0时，此时两个字符串也是相等得，memcmp同理
+  return *s1 == *s2 || n == 0 ? 0 : (unsigned char)*s1 < (unsigned char)*s2 ? -1 : 1;
+  //panic("Not implemented");
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+    unsigned char *p = s;
+    while (n--) {
+        *p++ = (unsigned char)c;
+    }
+    return s;
+  //panic("Not implemented");
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +111,19 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  if(!n)
+    {
+        return 0;
+    }
+    // 当比较位数不为0时，且每位数据相等时，移动指针
+    while(n-- && *(char*)s1 == *(char*)s2)
+    {
+        s1 = (char*)s1 + 1;    // 转换类型，移动指针
+        s2 = (char*)s2 + 1;
+    }
+    // 返回超过比较位数之后 比较的大小
+    return( *((unsigned char *)s1) - *((unsigned char *)s2) );   
+  //panic("Not implemented");
 }
 
 #endif
