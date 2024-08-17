17f0e158ba345366294030fcf202c9477c9fd0bc
tracer-ics2023 <tracer@njuics.org>
2024-08-18 02:52:46 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:52:46 up 42 min,  1 user,  load average: 1.74, 1.10, 0.75

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -74,6 +74,12 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+    if (vgactl_port_base[1] != 0) {
+    // 调用 update_screen() 更新屏幕
+    update_screen();
+    // 将同步寄存器清零
+    vgactl_port_base[1] = 0;
+  }
 }
 
 void init_vga() {
