5d57bb114372dee8514ffe55b449cfce85c97aad
tracer-ics2023 <tracer@njuics.org>
2024-08-19 11:54:22 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:54:22 up  2:09,  1 user,  load average: 0.88, 0.79, 0.80

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -125,6 +125,7 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
+          isa_reg_display();
       // fall through
     case NEMU_QUIT: statistic();
   }
