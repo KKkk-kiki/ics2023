4057f495b4fd8ae28dd109eac4cd1473dd47897f
tracer-ics2023 <tracer@njuics.org>
2024-08-19 15:54:41 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:54:41 up  6:09,  1 user,  load average: 0.87, 0.72, 0.77

--- nanos-lite/include/proc.h
+++ nanos-lite/include/proc.h
@@ -17,5 +17,6 @@ typedef union {
 } PCB;
 
 extern PCB *current;
-
+void naive_uload(PCB *pcb, const char *filename);
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 #endif
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <klib.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -10,8 +11,33 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  // TODO();
+    Elf_Ehdr ehdr;
+    ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+
+    // 检查ELF魔数，确保这是一个有效的ELF文件
+    //7f 45 4c 46 02 01 01 00 00 00 00 00 00 00 00 00
+    // if (memcmp(ehdr->e_ident, ELFMAG, SELFMAG) != 0) {
+    //     panic("Not a valid ELF file");
+    // }
+
+    // 读取程序头表
+    Elf_Phdr phdr[ehdr.e_phnum];
+    ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
+
+    // 遍历程序头表，加载各个段到内存中
+    for (int i = 0; i < ehdr.e_phnum; i++) {
+      if (phdr[i].p_type == PT_LOAD) {
+        // 写入段在内存中的地址
+        ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+        // 如果内存大小大于文件大小，将剩余部分清零
+        if (phdr[i].p_memsz > phdr[i].p_filesz) {
+          memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+        }
+      }
+    }
+
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
