192395ef74c899e1a814d3136b549e1541fdbf00
tracer-ics2023 <tracer@njuics.org>
2024-08-19 22:43:09 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:43:09 up 12:58,  1 user,  load average: 0.57, 0.58, 0.67

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -65,23 +65,22 @@ int _open(const char *path, int flags, mode_t mode) {
   return 0;
 }
 
-// int _write(int fd, void *buf, size_t count) {
-//   if (fd == 1 || fd == 2){
-//     printf("writewrite!!!!!!!!!\n");
-//     int ret = _syscall_(SYS_write, (intptr_t)buf, count, 0);
-//     printf("ret:  %d",ret);
-//   }
-//   // _exit(SYS_write);
-//   return ret;
-// }
+int _write(int fd, void *buf, size_t count) {
+  if (fd == 1 || fd == 2){
+    int ret = _syscall_(SYS_write, (intptr_t)buf, count, 0);
+    return ret;
+  }
+  // _exit(SYS_write);
+  return 0;
+}
 extern char end;
 intptr_t current_break= (intptr_t)&end;
 void *_sbrk(intptr_t increment) {
   intptr_t old_break = current_break;
-  intptr_t new_break = current_break + increment
+  intptr_t new_break = current_break + increment;
   if(_syscall_(SYS_brk, new_break, 0 , 0)==0){
       current_break = new_break;
-      return void *old_break;
+    return (void *)old_break;
   }
   return (void *)-1;
 }
