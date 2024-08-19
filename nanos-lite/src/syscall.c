4bfe46a638901578896a8f3f33a98a86e38745eb
tracer-ics2023 <tracer@njuics.org>
2024-08-20 02:22:26 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:22:26 up 16:37,  1 user,  load average: 0.44, 0.80, 0.87

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,6 @@
 #include <common.h>
 #include "syscall.h"
+#include <fs.h>
 extern void yield();
 void halt(int code);
 uintptr_t sys_yield(Context *c){
@@ -21,6 +22,18 @@ int sys_brk(intptr_t addr){
   return 0;
 }
 
+int sys_open(char *pathname, int flags, int mode){
+  return fs_open(pathname,  flags,  mode);
+}
+int sys_close(int fd){
+  return fs_close(fd);
+};
+size_t sys_read(int fd, void *buf, size_t len){
+  return fs_read( fd, buf, len);
+}
+size_t sys_lseek(int fd, size_t offset, int whence){
+  return fs_lseek(fd, offset,  whence);
+}
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -37,6 +50,15 @@ void do_syscall(Context *c) {
       c->GPRx = sys_write(a[1],a[2]);break;
     case SYS_brk:
       c->GPRx = sys_brk(a[1]);break;
+    case SYS_open:
+      c->GPRx = sys_open((char*)a[1], a[2], a[3]);break;
+    case SYS_close:
+      c->GPRx = sys_close(a[1]);break;
+    case SYS_read:
+      c->GPRx = sys_read(a[1], (void *)a[2], a[3]);break;
+    case SYS_lseek:
+      c->GPRx = sys_lseek(a[1], a[2], a[3]);break;
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
