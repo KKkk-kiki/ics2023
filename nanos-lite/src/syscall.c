75dc864b7306982caa9d0ee8ab2164cdee6597c0
tracer-ics2023 <tracer@njuics.org>
2024-08-19 22:15:55 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:15:55 up 12:31,  1 user,  load average: 1.08, 0.83, 0.67

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -31,6 +31,7 @@ void do_syscall(Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
+  printf("syscall ID: %d",a[0]);
   switch (a[0]) {
     case SYS_yield:
       c->GPRx = sys_yield(NULL);break;
