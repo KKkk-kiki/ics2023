19605368a21e537cb379e1c357516d60f8ea0258
tracer-ics2023 <tracer@njuics.org>
2024-08-19 11:50:27 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:50:27 up  2:05,  1 user,  load average: 0.94, 0.98, 0.86

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -20,9 +20,9 @@ Context* __am_irq_handle(Context *c) {
   //   printf("reg : %lu\n",c->gpr[i]);
   // }
 
-  // printf("mcause :%lu\n",c->mcause);
+  printf("mcause :%lu\n",c->mcause);
   // printf("mepc :%lu\n",c->mepc);
-  printf("mstatus :%lu\n",c->mstatus);
+  // printf("mstatus :%lu\n",c->mstatus);
   // printf("pdir :%lu\n",c->pdir);
   return c;
 }
