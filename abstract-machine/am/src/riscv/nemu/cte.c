32155d70f14f529245874dd79f683336f98779f2
tracer-ics2023 <tracer@njuics.org>
2024-08-20 02:27:04 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:27:04 up 16:42,  1 user,  load average: 0.58, 0.64, 0.78

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -36,6 +36,7 @@ Context* __am_irq_handle(Context *c) {
   // printf("pdir :%lu\n",c->pdir);
   if (user_handler) {
     Event ev = {0};
+    printf("cause: %d\n",c->mcause);
     switch (c->mcause) {
       case 0: case 1: case 3:case 4:case 5:case 6:case 7:case 8:case 9:case 10:case 11:case 12:case 13:case 14:case 15:case 16:case 17:case 18:case 19: 
         ev.event = EVENT_SYSCALL;break;
