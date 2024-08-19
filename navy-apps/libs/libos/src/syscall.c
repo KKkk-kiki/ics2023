7496a3f40d21dbd3c7c1758122c3138a71bfc61f
tracer-ics2023 <tracer@njuics.org>
2024-08-19 22:33:06 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:33:06 up 12:48,  1 user,  load average: 1.16, 0.95, 0.82

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -67,6 +67,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   if (fd == 1 || fd == 2){
+    printf("writewrite!!!!!!!!!\n");
     int ret = _syscall_(SYS_write, (intptr_t)buf, count, 0);
     printf("ret:  %d",ret);
   }
