ce8ab3f7cc1be0ce5772327cbffd1a32d9ec3d31
tracer-ics2023 <tracer@njuics.org>
2024-08-20 15:28:26 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:28:26 up  4:35,  1 user,  load average: 1.08, 0.68, 0.71

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,6 +22,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T kbd = io_read(AM_INPUT_KEYBRD);
+  printf("keycode %d\n",kbd.keycode);
   return snprintf((char*)buf,len,"%s %s\n",kbd.keydown ?"kd":"ku",keyname[kbd.keycode]);
 }
 
