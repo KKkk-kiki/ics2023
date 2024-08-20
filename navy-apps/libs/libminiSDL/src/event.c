15e59cb92a4f911a549d19e614b0ed9277de19a3
tracer-ics2023 <tracer@njuics.org>
2024-08-21 01:08:21 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:08:21 up 14:15,  1 user,  load average: 0.95, 0.97, 0.89

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -24,22 +24,25 @@ int SDL_WaitEvent(SDL_Event *event) {
   char *k_type;
   char *k_name;
   // ((char*)buf,len,"%s %s",ev.keydown ?"kd":"ku",keyname[ev.keycode]);
-  if(NDL_PollEvent(buf, sizeof(buf))){
-      sscanf(buf,"%s %s",k_type,k_name);
-      if(strcmp(k_type,"kd") ==0 ){
-        event->type == SDL_KEYDOWN;
-      }
-      if(strcmp(k_type,"ku") ==0 ){
-        event->type == SDL_KEYUP;
-      }
-      for(int i = 0; i < sizeof(keyname)/sizeof(char *);i++){
-        if(strcmp(keyname[i],k_name)==0){
-          event->key.keysym.sym = i;
-          break;
-        }
-      }
-    return 1;
-  }
+  NDL_PollEvent(buf, sizeof(buf));
+  sscanf(buf,"%s %s",k_type,k_name);
+  printf("%s %s\n",k_type,k_name);
+  // if(NDL_PollEvent(buf, sizeof(buf))){
+  //     sscanf(buf,"%s %s",k_type,k_name);
+  //     if(strcmp(k_type,"kd") ==0 ){
+  //       event->type == SDL_KEYDOWN;
+  //     }
+  //     if(strcmp(k_type,"ku") ==0 ){
+  //       event->type == SDL_KEYUP;
+  //     }
+  //     for(int i = 0; i < sizeof(keyname)/sizeof(char *);i++){
+  //       if(strcmp(keyname[i],k_name)==0){
+  //         event->key.keysym.sym = i;
+  //         break;
+  //       }
+  //     }
+  //   return 1;
+  // }
   return 0;
 }
 
