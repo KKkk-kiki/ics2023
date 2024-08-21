bbb099c93f1000ac17735b336e907903d120bae8
tracer-ics2023 <tracer@njuics.org>
2024-08-21 17:13:40 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:13:40 up  3:00,  1 user,  load average: 1.45, 1.48, 1.43

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -16,7 +16,7 @@ static void sh_printf(const char *format, ...) {
 
 static void sh_banner() {
   // sh_printf("Built-in Shell in NTerm (NJU Terminal)\n\n");
-  sh_printf("1234567\n\n");
+  sh_printf("11\n\n");
 }
 
 static void sh_prompt() {
