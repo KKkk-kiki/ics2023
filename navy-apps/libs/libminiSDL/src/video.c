d6f0f79b7e212c7914110b50ef2c6b1bffcbe718
tracer-ics2023 <tracer@njuics.org>
2024-08-22 00:44:49 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:44:49 up 10:31,  1 user,  load average: 0.76, 0.98, 1.06

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -195,7 +195,8 @@ void SDL_FreeSurface(SDL_Surface *s) {
 }
 
 SDL_Surface* SDL_SetVideoMode(int width, int height, int bpp, uint32_t flags) {
-  if (flags & SDL_HWSURFACE) NDL_OpenCanvas(&width, &height);
+  int w = 0;int h=0;
+  if (flags & SDL_HWSURFACE) NDL_OpenCanvas(&w, &h);
   return SDL_CreateRGBSurface(flags, width, height, bpp,
       DEFAULT_RMASK, DEFAULT_GMASK, DEFAULT_BMASK, DEFAULT_AMASK);
 }
