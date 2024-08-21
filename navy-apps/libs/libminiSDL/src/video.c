b56594cb5be0a6acfc74ca7e9bdad099722bf247
tracer-ics2023 <tracer@njuics.org>
2024-08-21 19:01:18 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:01:18 up  4:48,  1 user,  load average: 0.95, 1.12, 0.94

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -56,7 +56,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     int h = 0;
   //设置画布全屏幕
     NDL_OpenCanvas(&w, &h);
-    NDL_DrawRect((uint32_t *)dst->pixels, dstX, dstY, width, height) ;
+    NDL_DrawRect(dst->pixels, dstX, dstY, width, height) ;
 
 }
 
