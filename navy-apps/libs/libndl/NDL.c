4753d4b327dd6db282d7f8dc3fa0a0362a199117
tracer-ics2023 <tracer@njuics.org>
2024-08-22 01:49:32 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:49:32 up 11:36,  1 user,  load average: 0.81, 0.48, 0.51

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -83,8 +83,8 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   for (int i = 0; i < h && y + i < canvas_h ; i++){
     printf("pass : %d\n",i);
     lseek(fd,((y + i +canvas_y) * screen_w + canvas_x + x),0);
-    printf("dd : %d\n",i);
     write(fd,pixels + i*w,( w < canvas_w-x ? w : canvas_w-x));
+    printf("dd : %d\n",i);
   }
   printf("pass\n");
   close(fd);
