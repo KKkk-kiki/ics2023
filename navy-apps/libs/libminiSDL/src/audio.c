63f1c257a37e0025f0cfd6556c8f258e7c9a9fa1
tracer-ics2023 <tracer@njuics.org>
2024-08-21 17:32:15 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:32:15 up  3:19,  1 user,  load average: 1.15, 1.00, 1.12

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -2,35 +2,35 @@
 #include <SDL.h>
 
 int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained) {
-  printf("Not implemented");
+  printf("Audio not implemented\n");
   return 0;
 }
 
 void SDL_CloseAudio() {
-  printf("Audio implemented");
+  printf("Audio not implemented\n");
 }
 
 void SDL_PauseAudio(int pause_on) {
-  printf("Audio implemented");
+  printf("Audio not implemented\n");
 }
 
 void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
-  printf("Audio implemented");
+  printf("Audio not implemented\n");
 }
 
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len) {
-  printf("Audio implemented");
+  printf("Audio not implemented\n");
   return NULL;
 }
 
 void SDL_FreeWAV(uint8_t *audio_buf) {
-  printf("Audio implemented");
+  printf("Audio not implemented\n");
 }
 
 void SDL_LockAudio() {
-  printf("Audio implemented");
+  printf("Audio not implemented\n");
 }
 
 void SDL_UnlockAudio() {
-  printf("Audio implemented");
+  printf("Audio not implemented\n");
 }
