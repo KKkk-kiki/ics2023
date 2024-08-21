0acf3314950eb9100ed884f8f27f2271b856e180
tracer-ics2023 <tracer@njuics.org>
2024-08-21 20:33:33 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:33:33 up  6:20,  1 user,  load average: 0.91, 0.37, 0.28

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -35,8 +35,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     int height = srcRect.h;
     //  printf("%d %d %d %d %d %d\n",srcX,srcY,dstX,dstY,width,height);
     // 获取源表面和目标表面的像素数据
-    // uint8_t *srcPixels = (uint8_t *)src->pixels;
-    // uint8_t *dstPixels = (uint8_t *)dst->pixels;
+    uint32_t *srcPixels = (uint32_t *)src->pixels;
+    uint32_t *dstPixels = (uint32_t *)dst->pixels;
 
     // 计算每像素字节数
     int bytesPerPixel = src->format->BytesPerPixel;
