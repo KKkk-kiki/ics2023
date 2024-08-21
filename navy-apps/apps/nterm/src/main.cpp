5c51265ff66ffd60afb5367f415c64021462c0b2
tracer-ics2023 <tracer@njuics.org>
2024-08-22 01:23:14 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:23:14 up 11:10,  1 user,  load average: 0.53, 0.57, 0.66

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]) {
   int win_w = font->w * W;
   int win_h = font->h * H;
   screen = SDL_SetVideoMode(win_w, win_h, 32, SDL_HWSURFACE);
-  // screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
+  // screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE); 设为全屏
   term = new Terminal(W, H);
   if (argc < 2) { builtin_sh_run(); }
   else { extern_app_run(argv[1]); }
