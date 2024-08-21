b4ff9ac2f000c75c595df75f3cd7a2543448e616
tracer-ics2023 <tracer@njuics.org>
2024-08-21 13:27:36 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:27:36 up  4:16,  1 user,  load average: 0.72, 0.70, 0.45

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -19,8 +19,6 @@ static int canvas_y;
 // 以毫秒为单位返回系统时间
 uint32_t NDL_GetTicks(struct timeval *tv, struct timezone *tz) {
   int result =  gettimeofday(tv, tz);
-    printf("here!!\n");
-    // printf("start :%u\n",start_ticks);
   uint32_t ticks = (tv->tv_sec * 1000) + (tv->tv_usec / 1000);
   return ticks;
 }
