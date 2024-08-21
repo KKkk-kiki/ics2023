b0624bfbdff926a961f68e1bd94f74a8aee46602
tracer-ics2023 <tracer@njuics.org>
2024-08-21 15:23:18 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:23:18 up  1:10,  1 user,  load average: 1.09, 0.89, 0.85

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -28,8 +28,8 @@ static void sh_handle_cmd(const char *cmd) {
 void builtin_sh_run() {
   sh_banner();
   sh_prompt();
-
-  while (1) {
+  int i = 1;
+  while (i) {
         
     SDL_Event ev;
     if (SDL_PollEvent(&ev)) {
@@ -42,6 +42,6 @@ void builtin_sh_run() {
       }
     }
     refresh_terminal();
-    SDL_Delay(500);
+    i--;
   }
 }
