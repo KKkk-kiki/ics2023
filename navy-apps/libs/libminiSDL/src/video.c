5804c7d1f294ea92a0b0d46d209aeb11dae1ddd6
tracer-ics2023 <tracer@njuics.org>
2024-08-23 02:33:14 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:33:14 up 16:35,  1 user,  load average: 1.48, 1.18, 1.11

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -119,7 +119,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     uint32_t *dstPixels = (uint32_t *)dst->pixels;
     uint8_t *sPixels = (uint8_t *)s->pixels;
     // 遍历源表面的每个像素
-    printf("%d %d %d %d %d %d\n",x,y,w,h,s->w,s->h);
+    // printf("%d %d %d %d %d %d\n",x,y,w,h,s->w,s->h);
     for (int i = 0; i < s->h; i++) {
         for (int j = 0; j < s->w; j++) {
             // 获取像素索引
