1443a152eb853260db979c4d164c76b35e622bea
tracer-ics2023 <tracer@njuics.org>
2024-08-22 21:53:47 +0800

    >  run NEMU 2579373189 kk Linux KK 5.15.0-117-generic #127~20.04.1-Ubuntu SMP Thu Jul 11 15:36:12 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:53:47 up 11:55,  1 user,  load average: 0.43, 0.62, 0.81

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -115,7 +115,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
      // 创建一个新的32位深度的SDL_Surface
     SDL_Surface *dst = SDL_CreateRGBSurface(0, s->w, s->h, 32, 0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF);
     assert(dst);
-    assert(dst->w ==w);
+    printf("%d %d\n",dst->w,s->w);
     SDL_Palette *palette = s->format->palette;
     uint32_t *dstPixels = (uint32_t *)dst->pixels;
     uint8_t *sPixels = (uint8_t *)s->pixels;
